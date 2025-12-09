9f725d3fd7e964ca158b3006960f2f64ec0f7fc0
leyvsi <sylviecosta78500@gmail.com>
2025-12-09 15:23:52 +0000

    update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,18 +1,47 @@
-#include <stdlib.h>
-#include "binary_trees.h"
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+#include <stddef.h>
+
+/* Binary tree node structure */
+
+struct binary_tree_s
 {
-binary_tree_t *node;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
+};
 
-node = malloc(sizeof(binary_tree_t));
-if (node == NULL)
-return NULL;
+typedef struct binary_tree_s binary_tree_t;
+typedef struct struct binary_tree_s bst_t;
+typedef struct struct binary_tree_s avl_t;
+typedef struct struct binary_tree_s heap_t;
 
-node->n = value;
-node->parent = parent;
-node->left = NULL;
-node->right = NULL;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *node);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
-return node;
-}
+#endif
+
