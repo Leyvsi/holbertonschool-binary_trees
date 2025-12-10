d82e3873ee2c962a90ee7083f1402674df8e7847
leyvsi <sylviecosta78500@gmail.com>
2025-12-10 14:09:41 +0100

    update binary_trees.h add betty style

--- binary_trees.h
+++ binary_trees.h
@@ -3,8 +3,23 @@
 
 #include <stddef.h>
 
-/* Binary tree node structure */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
+typedef struct binary_tree_s binary_tree_t;
 struct binary_tree_s
 {
 int n;
