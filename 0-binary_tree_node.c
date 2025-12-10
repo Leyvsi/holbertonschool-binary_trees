681c1c3916ea123f3ea14168692ef396a3d751bb
leyvsi <sylviecosta78500@gmail.com>
2025-12-10 13:41:42 +0000

    update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,7 +6,8 @@
 *
 *@value: Value to store in the new node
 *
-*Return: Pointer to the new node or NULL on failure */
+*Return: Pointer to the new node or NULL on failure
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
