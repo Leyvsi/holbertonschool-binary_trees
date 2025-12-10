347b156a03bb77137a4d77d216236563a8d11e8a
leyvsi <sylviecosta78500@gmail.com>
2025-12-10 14:36:13 +0000

    add tasks 12 to tasks 15

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
-/** binary_tree_insert_left - Inserts a node as left-child of parent
-*@parent: Pointer to the parent node
-*
-*@value: Value to store in the new node
-*
-*Return: Pointer to the new node or NULL on failure
-*/
+/**
+ * binary_tree_insert_left - Inserts a node as the left child of another node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the new node or NULL on failure
+ */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,13 +1,12 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_insert_left - Inserts a node as the left child of another node
-*@parent: Pointer to the parent node
-*
-*@value: Value to store in the new node
-*
-*Return: Pointer to the new node or NULL on failure
-*/
+ * binary_tree_insert_left - Inserts a node as the left child of another node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the new node or NULL on failure
+ */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
